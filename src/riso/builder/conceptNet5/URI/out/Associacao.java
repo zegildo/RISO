a7ddc0acb8f22b6cb04d5fb426519cb124e0b348
Jose Gildo <zegildo@gmail.com>
2012-08-17 02:49:20 -0300

    Acrescimo de novos arquivos

---
+++ src/riso/builder/conceptNet5/URI/out/Associacao.java
@@ -0,0 +1,33 @@
+package riso.builder.conceptNet5.URI.out;
+
+import java.util.List;
+
+public class Associacao {
+
+	private List<List<String>> terms;
+	//private List<ElementoAssociacao> similar;
+
+	public List<List<String>> getTerms() {
+		return terms;
+	}
+
+	public void setTerms(List<List<String>> terms) {
+		this.terms = terms;
+	}
+
+	
+/*
+	public List<ElementoAssociacao> getSimilar() {
+		return similar;
+	}
+
+	public void setSimilar(List<ElementoAssociacao> similar) {
+		this.similar = similar;
+	}
+
+	@Override
+	public String toString() {
+		return "Associacao [terms=" + terms + ", similar=" + similar + "]";
+	}*/
+	
+}
---
+++ src/riso/builder/conceptNet5/URI/out/ElementoAssociacao.java
@@ -0,0 +1,22 @@
+package riso.builder.conceptNet5.URI.out;
+
+
+public class ElementoAssociacao {
+
+	private String termo;
+	private double valor;
+	public String getTermo() {
+		return termo;
+	}
+	public void setTermo(String termo) {
+		this.termo = termo;
+	}
+	public double getValor() {
+		return valor;
+	}
+	public void setValor(double valor) {
+		this.valor = valor;
+	}
+	
+	
+}
